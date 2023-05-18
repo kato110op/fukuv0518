6eb5a6cc05e0821699be63b21e5f6e86c6964a56
kato <130636687+kato110op@users.noreply.github.com>
2023-05-18 13:25:46 +0900

    Initial commit

---
+++ fukuv0518/Form1.cs
@@ -0,0 +1,25 @@
+namespace fukuv0518
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("â¡ì°Å@èπíà");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Left = 0;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button3.Visible = false;
+        }
+    }
+}
