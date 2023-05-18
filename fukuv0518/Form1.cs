aba682e8741070f8fefba5a2af19874ad128cd5e
kato <130636687+kato110op@users.noreply.github.com>
2023-05-18 13:58:04 +0900

    復習問題

--- fukuv0518/Form1.cs
+++ fukuv0518/Form1.cs
@@ -9,7 +9,7 @@ namespace fukuv0518
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("â¡ì°Å@èπíà");
+            button1.Text = "â¡ì°Å@èπíà";
         }
 
         private void button2_Click(object sender, EventArgs e)
